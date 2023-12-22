f440f9e70ac47bbab79323ef88b84576ac5edf15
Fabian Quintero <grovveip@gmail.com>
2023-12-22 16:00:23 -0500

    ok

--- resources/js/services/http_services.js
+++ resources/js/services/http_services.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export function http() {
     return axios.create({
-        // baseURL: 'http://127.0.0.1:8000/'
+        baseURL: 'http://127.0.0.1:8000/'
         // baseURL: 'https://192.168.1.78/'
-        baseURL: 'https://caracterizacionafro-elpaso.com/'
+        // baseURL: 'https://caracterizacionafro-elpaso.com/'
     });
 }
